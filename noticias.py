2d6cac849057205e59d64b2b079349f963da24f8
Diogo Paim_personal_account <paimlab@gmail.com>
2026-09-09 12:55:58 -0300

    Update noticias.py

--- noticias.py
+++ noticias.py
@@ -1,51 +1,24 @@
-import feedparser
 from datetime import datetime
 
-RSS_FEEDS = {
-    "Supply Chain": "https://news.google.com/rss/search?q=supply+chain&hl=pt-BR&gl=BR&ceid=BR:pt-419",
-    "Intralogística": "https://news.google.com/rss/search?q=intralogistica&hl=pt-BR&gl=BR&ceid=BR:pt-419",
-    "Armazenagem": "https://news.google.com/rss/search?q=armazenagem&hl=pt-BR&gl=BR&ceid=BR:pt-419"
-}
+html = f"""
+<!DOCTYPE html>
+<html>
+<body>
 
-noticias_html = ""
+<h1>Radar Logística & Intralogística</h1>
 
-for categoria, feed_url in RSS_FEEDS.items():
+<p>
+Última atualização:
+{datetime.now()}
+</p>
 
-    feed = feedparser.parse(feed_url)
+<h2>TESTE OK</h2>
 
-    for item in feed.entries[:5]:
+</body>
+</html>
+"""
 
-        noticias_html += f"""
-        <div style="background:white;padding:15px;margin:10px 0;border-radius:8px;">
-            <h3>
-                <a href="{item.link}" target="_blank">
-itle}
-                </a>
-            </h3>
+with open("index.html","w",encoding="utf-8") as f:
+    f.write(html)
 
-            <small>{categoria}</small>
-
-        </div>
-        """
-
-data = datetime.now().strftime("%d/%m/%Y %H:%M")
-
-with open("index.html", "r", encoding="utf-8") as arquivo:
-
-    html = arquivo.read()
-
-html = html.replace(
-    "<!-- ULTIMA_ATUALIZACAO -->",
-    data
-)
-
-html = html.replace(
-    "<!-- NOTICIAS_AQUI -->",
-    noticias_html
-)
-
-with open("index.html", "w", encoding="utf-8") as arquivo:
-
-    arquivo.write(html)
-
-print("Notícias atualizadas.")
+print("Teste concluído")
