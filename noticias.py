b9b19ba7b710eb74dd10f1788fe526dd79b98700
Diogo Paim_personal_account <paimlab@gmail.com>
2026-09-09 14:31:28 -0300

    Update noticias.py

--- noticias.py
+++ noticias.py
@@ -1,28 +1,10 @@
-from datetime import datetime
-
-html = f"""
 <!DOCTYPE html>
 <html>
-<head>
-<title>Radar Logístico</title>
-</head>
-
 <body>
 
-<h1>Radar Logística & Intralogística</h1>
+<h1>TESTE INDEX</h1>
 
-<p>
-Última atualização:
-{datetime.now()}
-</p>
-
-<h2>TESTE OK</h2>
+<h2 style="color:red">FUNCIONOU</h2>
 
 </body>
 </html>
-"""
-
-with open("index.html", "w", encoding="utf-8") as f:
-    f.write(html)
-
-print("Teste concluído")
